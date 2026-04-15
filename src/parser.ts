b378fc0f53b135ed817d7bc437af1db6d30431e9
Davi de Farias Rinaldi <davi.rinaldi@triersistemas.com.br>
2026-04-15 17:45:13 -0300

    feat: add RESP simple string, error, and integer type parsing

--- src/parser.ts
+++ src/parser.ts
@@ -1,39 +1,55 @@
 export function parseRESP(data: string): string[] {
 
 
-const parser = data.split('\r\n');
+    const parser = data.split('\r\n');
 
-        let position = 0;
-        let numbers: number | null = null;
-        const results: string[] = []
-        while (position < parser.length) {
+    let position = 0;
+    let numbers: number | null = null;
+    const results: string[] = []
+    while (position < parser.length) {
 
 
 
-            if (parser[position]?.startsWith('*')) {
-                numbers = parseInt(parser[position]?.slice(1) ?? '0');
+        if (parser[position]?.startsWith('*')) {
+            numbers = parseInt(parser[position]?.slice(1) ?? '0');
 
-            }
-            if (numbers === null) break;
-            position++
-            {
-                for (let i = 0; i < numbers; i++) {
-
-                    if (parser[position]?.startsWith('$')) {
-                        position++
-                        results.push(parser[position] ?? '')
-                        position++
-
-                    }
-                }
-            }
         }
+        if (numbers === null) break;
+        position++
+        {
+            for (let i = 0; i < numbers; i++) {
 
-        return results;
+                if (parser[position]?.startsWith('$')) {
+                    position++
+                    results.push(parser[position] ?? '')
+                    position++
+
+                } else if (parser[position]?.startsWith('+')) {
+
+                    results.push(parser[position]?.slice(1) ?? '')
+                    position++
+
+                } else if (parser[position]?.startsWith('-')){
+                    results.push(parser[position]?.slice(1) ?? '')
+                    position++
+                }else if(parser[position]?.startsWith(':')){
+                    results.push(parser[position]?.slice(1) ?? '')
+                    position++
 
 
 
-
-
+            }
+        }}
+    }
+    return results;
 
 }
+
+
+
+
+
+
+
+
+
